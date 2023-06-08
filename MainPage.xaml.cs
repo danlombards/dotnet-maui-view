16832f6d8851ebcf008ee819619ea4a922e1f44c
Danilo Lombardi dos Santos <123340636+danlombards@users.noreply.github.com>
2023-06-08 00:20:02 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace ShareViewMaui;
+
+public partial class MainPage : ContentPage
+{
+    Page lixo = new LixoPage();
+
+	public MainPage()
+	{
+		InitializeComponent();
+        
+        //var graphics = (MauiContext as IMauiContext)?.Context?.GetGraphics();
+    }
+
+    private void OnOpenShareView(object sender, EventArgs e)
+	{
+       
+
+        Popup.Open(new DetailsPopup(pgImage));
+	}
+
+    private void OnOpenShareView2(object sender, EventArgs e)
+    {
+
+
+        Popup.Open(new DetailsPopup(pgImage2));
+    }
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Application.Current.MainPage.Navigation.PushAsync(lixo);
+    }
+
+}
+
